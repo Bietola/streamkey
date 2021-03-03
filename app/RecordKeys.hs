6e6520ebdfcbe1bb063026a60ea8ed4497978294
Bietola <dincio.montesi@gmail.com>
2021-03-03 20:52:50 +0100

    Aimlessly copy xlib window example

--- app/RecordKeys.hs
+++ app/RecordKeys.hs
@@ -1,10 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BlockArguments #-}
 
 module RecordKeys (recordKeys) where
 
-import Turtle
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
 
+import Graphics.X11 as X
+
+recordKeys :: IO ()
 recordKeys = do
-  rawPress <- inshell "python3 pyscripts/record_kps.py" stdin
+  dsp <- X.openDisplay ""
+  print "Display opened"
 
-  liftIO $ print ("rawpress: " <> rawPress)
+  let whiteColor = whitePixel dsp (X.defaultScreen dsp)
+  let blackColor = blackPixel dsp (X.defaultScreen dsp)
+
+  win <- X.createSimpleWindow dsp (X.defaultRootWindow dsp) 0 0 200 100 0 blackColor blackColor
+
+  X.selectInput dsp win X.keyPressMask
+
+  X.mapWindow dsp win
+
+  gc <- X.createGC dsp win
+
+  X.setForeground dsp gc whiteColor
+
+  X.allocaXEvent \eptr -> forever do
+    print "Waiting for event..."
+    X.nextEvent dsp eptr
+
+    print "Got it!"
+    eventType <- X.get_EventType eptr
+    print eventType
